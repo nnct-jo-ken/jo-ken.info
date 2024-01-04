659bd957894c171c6389b636f04c584cbe34ff39
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 01:08:24 +0900

    feat: フォントと最小高さを設定

---
+++ src/styles/global-style.css.ts
@@ -0,0 +1,7 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("html, body", {
+	fontFamily:
+		'"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif',
+	minHeight: "100dvh",
+});
