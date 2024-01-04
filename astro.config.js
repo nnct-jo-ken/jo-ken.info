135abb8034473944ec29fc7222e86bfa7ef2a6f8
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 01:57:04 +0900

    build: astro-iconを追加

--- astro.config.js
+++ astro.config.js
@@ -1,13 +1,14 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	build: {
 		format: "file",
 	},
-	integrations: [compress()],
+	integrations: [compress(), icon()],
 	vite: {
 		plugins: [vanillaExtractPlugin()],
 	},
