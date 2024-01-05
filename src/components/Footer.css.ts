3017cfde340b416788d81517859bcc0ac9a50781
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 15:20:08 +0900

    feat: フッターを追加

---
+++ src/components/Footer.css.ts
@@ -0,0 +1,18 @@
+import { style } from "@vanilla-extract/css";
+
+import { theme } from "~/styles/theme.css";
+import { color } from "~/styles/utils.css";
+
+export const footer = style({
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	gap: "16px",
+
+	width: "100%",
+
+	padding: "16px",
+
+	borderTop: `1px solid ${color(theme.color.border)}`,
+});
