0ce7286ca8a43f8ff0060bf729d643a42d7fecef
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 17:26:32 +0900

    fix: サイト名を修正

--- astro.config.js
+++ astro.config.js
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://jo-ken.net",
+	site: "https://jo-ken.info/",
 	build: {
 		format: "file",
 	},
