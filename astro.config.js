d439ff9ff17caa75c0692e2753253f47452db7e5
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 16:51:53 +0900

    feat: メタタグを追加

--- astro.config.js
+++ astro.config.js
@@ -1,3 +1,4 @@
+import sitemap from "@astrojs/sitemap";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
@@ -5,10 +6,11 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://jo-ken.net",
 	build: {
 		format: "file",
 	},
-	integrations: [compress(), icon()],
+	integrations: [compress(), icon(), sitemap()],
 	vite: {
 		plugins: [vanillaExtractPlugin()],
 	},
