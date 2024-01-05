80a51893c21823a7336258a4c0da2ed66360a72a
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 14:17:09 +0900

    chore: ESLintのエラーを修正

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
 import { shun_shobon } from "@shun-shobon/eslint-config";
 
-export default shun_shobon();
+export default shun_shobon(
+	{},
+	{
+		files: ["**/*.astro"],
+		languageOptions: {
+			globals: {
+				Astro: false,
+				Fragment: false,
+			},
+		},
+	},
+);
