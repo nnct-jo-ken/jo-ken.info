05f3771657a442f3bbcdfc16708e04d2d9555a7d
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-05 11:00:41 +0900

    feat: ヒーローを追加

---
+++ src/components/Hero.css.ts
@@ -0,0 +1,64 @@
+import { style } from "@vanilla-extract/css";
+
+import * as queries from "~/styles/media-queries.css";
+import { vars } from "~/styles/theme.css";
+import { color } from "~/styles/utils.css";
+
+export const hero = style({
+	width: "100%",
+	height: "100svh",
+
+	position: "relative",
+});
+
+export const image = style({
+	"position": "absolute",
+	"top": 0,
+	"left": 0,
+
+	"width": "calc(100% - 48px)",
+	"height": "calc(100% - 64px)",
+
+	"objectFit": "cover",
+
+	"@media": {
+		[queries.md]: {
+			inset: "unset",
+			right: 0,
+		},
+	},
+});
+
+export const copy = style({
+	"position": "absolute",
+	"top": `calc(${vars.size.header} + 24px)`,
+	"right": "24px",
+
+	"display": "flex",
+	"flexDirection": "column",
+	"alignItems": "start",
+	"gap": "8px",
+
+	"color": color(vars.color.background),
+	"fontSize": "5.5vh",
+	"fontFeatureSettings": "'vchw' 1",
+	"writingMode": "vertical-rl",
+
+	"@media": {
+		[queries.md]: {
+			inset: "unset",
+			left: "24px",
+			bottom: "24px",
+
+			fontSize: "clamp(2rem, 6vw, 5rem)",
+			fontFeatureSettings: "'palt' 1",
+			writingMode: "unset",
+		},
+	},
+});
+
+export const copyLine = style({
+	paddingInline: "8px",
+
+	backgroundColor: color(vars.color.primary),
+});
--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -5,6 +5,7 @@ export const vars = createGlobalTheme(":root", {
 		// colorはrgbで指定すること
 		// 参考: https://tailwindcss.com/docs/customizing-colors#using-css-variables
 		background: "255 255 255",
+		primary: "108 203 242",
 	},
 	size: {
 		header: "4rem",
