54ff37c2cf4dd147e7d4e130cebe46d09f51e285
NISHIZAWA Shuntaro <me@s2n.tech>
2024-01-09 20:53:41 +0900

    chore(deps): update dependency @shun-shobon/eslint-config to ^0.6.1

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,3 @@
 import { shun_shobon } from "@shun-shobon/eslint-config";
 
-export default shun_shobon(
-	{},
-	{
-		files: ["**/*.astro"],
-		languageOptions: {
-			globals: {
-				Astro: false,
-				Fragment: false,
-			},
-		},
-	},
-);
+export default shun_shobon();
